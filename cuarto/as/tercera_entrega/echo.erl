2efaf8f66d5e916ca5be996641db71cc16728e08
AdrianInsua <adrian.insua.y@gmail.com>
2015-09-22 16:21:37 +0200

    Entrega de la practica de erlang concorrente, con creacion de un proceso que se mantiene haciendo loop

---
+++ cuarto/as/tercera_entrega/echo.erl
@@ -0,0 +1,37 @@
+-module(echo).
+-export([start/0, stop/0, print/1]).
+-export([init/0]).
+
+%%Funcion para la creacion del proceso y su registro
+start() ->
+    Pid = spawn(?MODULE, init, []),
+    register(server, Pid).
+
+%%Funcion que inicia el loop pasando una lista vacia
+init() ->
+    loop().
+
+
+stop() ->
+    server ! {stop, self()},
+    receive
+        {ok, server} -> ok
+    end.
+
+print(Term) ->
+    server ! {print, Term, self()},
+    receive
+        {ok, server} -> ok
+    end.
+
+loop() ->
+    receive
+        {stop, From} ->
+            From ! {ok, server};
+        {print, Term, From} ->
+            io:format("~s~n", [Term]),
+            From ! {ok, server},
+            loop()
+    end.
+
+
